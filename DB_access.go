44baa889b401f9e4f5e95a7d21e0d48cfb840bae
pixal <pixal@pix-notebook.pixnet.com>
2021-03-25 00:38:02 -0300

    .

--- DB_access.go
+++ DB_access.go
@@ -173,3 +173,32 @@ func loginCheck(user string, pass string) bool {
 	}
 	return false
 }
+func ofCheck(user string, pass string) bool {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	collection := client.Database("CAE").Collection("usuarios")
+	filterCursor, err := collection.Find(ctx, bson.M{"user": user, "password": pass})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var episodesFiltered []bson.M
+	if err = filterCursor.All(ctx, &episodesFiltered); err != nil {
+		log.Fatal(err)
+	}
+	if episodesFiltered != nil {
+		return true
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return false
+}
--- remover.go
+++ remover.go
@@ -7,5 +7,10 @@ import (
 func Remover(w http.ResponseWriter, r *http.Request) {
 	id_user := r.FormValue("id_user") //buscar id del usuario y el id de la consulta para realizar la  eliminación de esta
 	id_consulta := r.FormValue("id_consulta")
+	userCookie, _ := r.Cookie("user")
+	userPw, _ := r.Cookie("pw")
+	if id_user == userCookie.Value && loginCheck(id_user,userPw.Value) && {
+		
+	}
 	print(id_user, id_consulta)
 }
