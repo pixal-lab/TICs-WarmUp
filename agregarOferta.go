ecd49bdd0e3ccf08463d86f40fbe50990791685c
pixal <pixal@pix-notebook.pixnet.com>
2021-03-29 00:22:11 -0300

    alerts

--- agregarOferta.go
+++ agregarOferta.go
@@ -14,13 +14,13 @@ func AgregarOferta(w http.ResponseWriter, r *http.Request) {
 	cuota, err2 := strconv.Atoi(r.FormValue("cuota"))
 	periodo, err3 := strconv.Atoi(r.FormValue("periodo"))
 	if err1 != nil && err2 != nil && err3 != nil {
-		fmt.Fprintf(w, "error")
+		fmt.Fprintf(w, "formato de datos no valido")
 	} else {
 		if validarDatosCalculo(total, cuota, periodo) {
 			addOferta(userCookie.Value, producto, vendedor, total, cuota, periodo, cae(total, cuota, periodo))
 			http.Redirect(w, r, "login.html", http.StatusSeeOther)
 		} else {
-			fmt.Fprintf(w, "error2")
+			fmt.Fprintf(w, "datos no validos")
 		}
 	}
 }
